4f4020548633308ceae802d5e1378fea876df914
xiaobxia <673806687@qq.com>
2021-02-21 10:09:27 +0800

    xg

--- src/views/infoFlow/index.vue
+++ src/views/infoFlow/index.vue
@@ -87,6 +87,7 @@
         label="操作">
         <template slot-scope="scope">
           <el-button type="text" @click="addNew(scope.row)">编辑</el-button>
+          <el-button type="text" @click="deleteRow(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -167,6 +168,22 @@ export default {
     },
     addNew(row) {
       this.$refs.editDialog.open(row || {})
+    },
+    deleteRow(row) {
+      this.$confirm('确认删除?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.post('fbsServer/infoFlow/deleteRecord', {
+          info_id: row.id
+        }).then(({ message }) => {
+          this.$message.success('成功')
+          this.reQueryList()
+        })
+      }).catch(err => {
+        console.log('err', err)
+      })
     }
   }
 }
